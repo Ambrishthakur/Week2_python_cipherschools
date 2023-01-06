ecacd94a39dca7c710842c39ee025e0cd75c9cfd
Ambrishthakur <114800245+Ambrishthakur@users.noreply.github.com>
2023-01-06 23:52:25 +0530

    Add files via upload

---
+++ 1string_vs_lists.py
@@ -0,0 +1,16 @@
+# lists vs strings
+
+# strings are immutable
+# lists are mutable
+
+s= "string"
+
+# t = s.title()
+# print(t)
+
+l = ['word1' , 'word2' , 'word3']
+
+l.pop() # it means remove last string from list.
+
+l.append("word4") # it means add word4 in list.
+print(l)
