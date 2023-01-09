7e342ef78ad17e00447d8c53d0a3bcae482acc14
Ambrishthakur <114800245+Ambrishthakur@users.noreply.github.com>
2023-01-09 23:20:26 +0530

    Add files via upload

---
+++ compare_lists.py
@@ -0,0 +1,9 @@
+# compare lists
+# == , is
+
+fruits1 = ['orange' , 'apple' , 'pear' ]
+fruits2 = [ 'banana' , 'kiwi' , 'apple' , 'banana']
+fruits3 = ['orange' , 'apple' , 'pear' ]
+
+print(fruits1 == fruits3) # (True) - [checks that values are same or not]
+print(fruits1 is fruits3)  # (False) - [checks that values placed in same or not]
